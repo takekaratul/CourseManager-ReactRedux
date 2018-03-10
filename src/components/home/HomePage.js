a51a6a3651164dffe35179835e87c989168e4461
Atul takekar <takekaratul@gmail.com>
2018-03-09 20:36:13 -0500

    added homepage and about

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className= "jumbotron">
+        <h1> Course Administrator</h1>
+        <p>React, Redux and React router in ES6 for responsive web apps</p>
+        <Link to= "about" className = "btn btn-primary btn-lg"> Learn more </Link>
+      </div>
+    );
+  }
+}
+
+export default HomePage

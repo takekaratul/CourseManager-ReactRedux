2c0accac05053fbee7a06d20f259f9222ddd8705
Atul takekar <takekaratul@gmail.com>
2018-03-10 22:07:33 -0500

    created actions

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,10 +1,40 @@
 import React, {PropTypes} from 'react';
 
 class CoursePage extends  React.Component{
+  constructor(props, context){
+    super(props, context);
+
+    this.state = {
+      course: {title: ""}
+    };
+
+    this.onClickSave = this.onClickSave.bind(this);
+    this.onTitleChange = this.onTitleChange.bind(this);
+  }
+
+  onTitleChange(event){
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave(){
+    alert(this.state.course.title);
+  }
   render(){
     return(
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input type = "text"
+               onChange={this.onTitleChange}
+               value = {this.state.course.title} />
+
+        <input type = "submit"
+               value = "Save"
+               onClick={this.onClickSave} />
+
+
       </div>
     );
   }
